c3045ad750da100cee93f638181437c1d5706807
Andrew Sheragy <andy@cerion.org>
2022-07-15 22:51:41 -0700

    updated final workout

--- src/data/programs/phase1.ts
+++ src/data/programs/phase1.ts
@@ -110,139 +110,36 @@ function getBenchDay(block: number, week: number): WorkoutNode {
 }
 
 function getUpperDay(block: number, week: number): WorkoutNode {
-  var pullupSets = [...Array(10).keys()].map(_ => {
-    const result: PersistedSet = {
-      weight: 0,
-      reps: 2,
-    };
-    return result;
-  });
-
   return {
-    name: 'Upper Week ' + (week + 1) + ' Block ' + (block + 1),
+    name: 'Upper Week ' + week + ' Block ' + block,
     lifts: [
-      {
-        name: 'Pullups',
-        key: 'pullups',
-        sets: pullupSets,
-      },
-      {
-        name: 'Dips',
-        key: 'dips',
-        sets: [
-          {
-            weight: 0,
-            reps: 6,
-          },
-          {
-            weight: 0,
-            reps: 6,
-          },
-          {
-            weight: 0,
-            reps: 6,
-          },
-          {
-            weight: 0,
-            reps: 6,
-          },
-          {
-            weight: 0,
-            reps: 6,
-          },
-        ],
-      },
-      {
-        name: 'Curls',
-        key: 'curls',
-        step: 10,
-        sets: [
-          {
-            weight: 60,
-            reps: 10,
-          },
-          {
-            weight: 60,
-            reps: 10,
-          },
-          {
-            weight: 60,
-            reps: 10,
-          },
-        ],
-      },
-      {
-        name: 'Incline DB Press',
-        key: 'inclineDb',
-        step: 2.5,
-        sets: [
-          {
-            weight: 30,
-            reps: 8,
-          },
-          {
-            weight: 30,
-            reps: 8,
-          },
-          {
-            weight: 30,
-            reps: 8,
-          },
-          {
-            weight: 30,
-            reps: 8,
-          },
-        ],
-      },
-      {
-        name: 'Face Pulls',
-      },
+      createPersisted('Pullups (3x5, 3x10)', 'pullups', createSets(0, 2, 6)),
+      createPersisted('Dips (5x10)', 'dips', createSets(0, 6, 5)),
+      createPersisted('Curls (3x20)', 'curls', createSets(60, 10, 3), 10),
+      createPersisted(
+        'Incline DB Press (3x20)',
+        'inclineDb',
+        createSets(30, 10, 3),
+        2.5,
+      ),
+      createPersisted('Face Pulls (2x30)', 'facePulls', createSets(50, 15, 2)),
     ],
   };
 }
 
 function getLowerDay(block: number, week: number): WorkoutNode {
   return {
-    name: 'Lower Week ' + (week + 1) + ' Block ' + (block + 1),
+    name: 'Lower Week ' + week + ' Block ' + block,
     lifts: [
-      {
-        name: 'Sumo DL (245x5/275x1)',
-        key: 'sumoDL',
-        sets: [
-          {
-            reps: 5,
-            weight: 205,
-          },
-          {
-            reps: 1,
-            weight: 225,
-          },
-        ],
-      },
-      {
-        name: 'Pause Squat (175x5)',
-        key: 'squat',
-        sets: [
-          {
-            reps: 5,
-            weight: 135,
-          },
-        ],
-      },
-      {
-        name: 'Pause Squat w/Bands (225x5/275x1)',
-        key: 'squatBands',
-        sets: [
-          {
-            reps: 5,
-            weight: 185,
-          },
-          {
-            reps: 1,
-            weight: 205,
-          },
-        ],
-      },
+      createPersisted('Sumo DL (225x10)', 'sumoDL', createSets(135, 10, 1)),
+      createPersisted(
+        'Front Squat (1x15)',
+        'frontSquat',
+        createSets(45, 10, 1),
+      ),
+      createPersisted('SSB Squat', 'ssb', createSets(95, 10, 1)),
+      createPersisted('Hatfield Squat', 'hatfield', createSets(135, 10, 1)),
+      createPersisted('RDL (3x15)', 'rdl', createSets(95, 10, 3)),
       {
         name: 'Calves',
       },
@@ -252,99 +149,55 @@ function getLowerDay(block: number, week: number): WorkoutNode {
 
 function getPushDay(block: number, week: number): WorkoutNode {
   return {
-    name: 'Push Week ' + (week + 1) + ' Block ' + (block + 1),
+    name: 'Push Week ' + week + ' Block ' + block,
     lifts: [
-      createPersisted('Overhead Press', 'ohp', createSets(100, 10, 5)),
-      {
-        name: 'Dumbell Bench Press',
-        key: 'dbPress',
-        sets: [
-          {
-            reps: 10,
-            weight: 50,
-          },
-          {
-            reps: 10,
-            weight: 60,
-          },
-          {
-            reps: 8,
-            weight: 70,
-          },
-          {
-            reps: 8,
-            weight: 65,
-          },
-          {
-            reps: 8,
-            weight: 60,
-          },
-        ],
-      },
-      createPersisted('Lat Raises', 'latRaise', createSets(20, 10, 3), 2.5),
-      createPersisted('Tricep Extensions', 'tricepExt', createSets(45, 15, 3)),
-      createPersisted('HS Press', 'hsPress', createSets(45 + 20, 10, 1)),
+      getPyramidLift(block, week, 'Overhead Press', 166),
+      createPersisted(
+        'Dumbell Bench Press (3x20)',
+        'dbPress',
+        createSets(55, 10, 3),
+      ),
+      createPersisted(
+        'Lat Raises (3x20)',
+        'latRaise',
+        createSets(15, 10, 3),
+        2.5,
+      ),
+      createPersisted(
+        'Tricep Extensions (3x30)',
+        'tricepExt',
+        createSets(35, 15, 3),
+      ),
+      createPersisted('HS Press (1x20)', 'hsPress', createSets(25, 10, 1)),
     ],
   };
 }
 
 function getPullDay(block: number, week: number): WorkoutNode {
   return {
-    name: 'Pull Week ' + (week + 1) + ' Block ' + (block + 1),
+    name: 'Pull Week ' + week + ' Block ' + block,
     lifts: [
-      {
-        name: 'Row Machine',
-        key: 'rows',
-        sets: [
-          {
-            weight: 45,
-            reps: 8,
-          },
-          {
-            weight: 45,
-            reps: 8,
-          },
-          {
-            weight: 45,
-            reps: 8,
-          },
-          {
-            weight: 45,
-            reps: 8,
-          },
-        ],
-      },
-      {
-        name: 'HS Pulldown (90+25 x 8-10)',
-        key: 'hsRow',
-        sets: [
-          {
-            weight: 90,
-            reps: 8,
-          },
-          {
-            weight: 90,
-            reps: 8,
-          },
-          {
-            weight: 90,
-            reps: 8,
-          },
-          {
-            weight: 90,
-            reps: 8,
-          },
-        ],
-      },
-      {
-        name: 'Hammer Curls',
-      },
-      {
-        name: 'Reverse Curls',
-      },
-      {
-        name: 'Reverse Lat Machine',
-      },
+      createPersisted('Dumbell Rows (5x10)', 'dbRows', createSets(80, 6, 5)),
+      createPersisted(
+        'HS Pulldown (3x15-20)',
+        'hsRow',
+        createSets(45 + 25, 10, 3),
+      ),
+      createPersisted(
+        'Reverse Curls (3x20)',
+        'reverseCurls',
+        createSets(55, 10, 3),
+      ),
+      createPersisted(
+        'Reverse Flys (3x20-30)',
+        'reverseCurls',
+        createSets(70, 15, 3),
+      ),
+      createPersisted(
+        'Hammer Curls (3x20)',
+        'hammerCurls',
+        createSets(25, 10, 3),
+      ),
     ],
   };
 }
@@ -383,6 +236,12 @@ function getStretch1() {
         name: 'Squat',
       },
       {
+        name: 'Hip Stretching',
+      },
+      {
+        name: 'Camel / Cow',
+      },
+      {
         name: 'Toe Touch',
       },
     ],
@@ -406,6 +265,12 @@ function getStretch2() {
         name: 'Squat',
       },
       {
+        name: 'Hip Stretching',
+      },
+      {
+        name: 'Camel / Cow',
+      },
+      {
         name: 'Toe Touch',
       },
     ],
@@ -414,20 +279,24 @@ function getStretch2() {
 
 export default function getProgram(): Program {
   const workouts: WorkoutNode[] = [];
+  var counter = 0;
 
   for (var block = 1; block <= 3; block++) {
     for (var week = 1; week <= 5; week++) {
-      //workouts.push(getStretch1());
+      workouts.push(counter++ % 2 == 0 ? getStretch1() : getStretch2());
       workouts.push(getDLDay(block, week));
-      //workouts.push(getBenchDay(block, week));
-      //workouts.push(getUpperDay(block, week));
-      //workouts.push(getStretch2());
-      //workouts.push(getLowerDay(block, week));
-      //workouts.push(getPushDay(block, week));
-      //workouts.push(getPullDay(block, week));
+      workouts.push(getBenchDay(block, week));
+
+      workouts.push(counter++ % 2 == 0 ? getStretch1() : getStretch2());
+      workouts.push(getUpperDay(block, week));
+      workouts.push(getLowerDay(block, week));
+
+      workouts.push(counter++ % 2 == 0 ? getStretch1() : getStretch2());
+      workouts.push(getPushDay(block, week));
+      workouts.push(getPullDay(block, week));
     }
     // Deload
-    //workouts.push(getDeloadDay());
+    workouts.push(getDeloadDay());
   }
 
   return {workouts};
