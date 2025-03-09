3b9ff93c4d53c903c9a730b434bdf8d0835c5e24
Andrew Sheragy <andy@cerion.org>
2025-03-09 09:46:45 -0700

    upgrade to RN 0.77

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,14 +2,12 @@
  * @format
  */
 
-import 'react-native';
 import React from 'react';
-import App from '../src/App';
+import ReactTestRenderer from 'react-test-renderer';
+import App from '../src/App.tsx';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  // Some issues with things like AsyncStorage if this needs to work
-  //renderer.create(<App />);
+test('renders correctly', async () => {
+  await ReactTestRenderer.act(() => {
+    ReactTestRenderer.create(<App />);
+  });
 });
